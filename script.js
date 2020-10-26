ca8f0823ae72bf0d94472aa62572ac6d7f42344f
DESKTOP-Q693PF6\vivi0 <yokoyama@public-function.co.jp>
2020-10-26 23:25:32 +0900

    宿題おわり

--- script.js
+++ script.js
@@ -3,29 +3,73 @@
 
 
 // Q 1
-
+let msg = document.getElementById('q1-text');
+console.log(msg.textContent);
 
 // Q 2
-
+let q2 = document.getElementById('q2-text');
+q2.textContent = 'ムーニーマン';
 
 // Q 3
-
+let q3 = document.getElementById('q3-text');
+q3.style.color = 'red';
 
 // Q 4 (難)
 // 「for of」を使います。
 
-// Q 5
+let q4 = document.querySelectorAll('.q4-text');
+for (text of q4) {
+	text.style.color = 'pink';
+}
 
+// Q 5
+let q5 = document.querySelectorAll('.q5-text');
+for (text of q5) {
+	text.style.background = 'yellow';
+}
 
 // Q 6
+let q6 = document.querySelector('#q6-text');
+	q6.addEventListener('click',function(){
+	this.style.color = 'red';
+})
 
 // Q 7
+let q7 = document.querySelector('#q7-text');
+	q7.addEventListener('mouseover',function(){
+	this.style.background = 'lightgray';
+	this.style.color = 'blue';
+})
 
 // Q 8
+let q8 = document.querySelector('#q8-text');
+	q8.addEventListener('mouseover',function(){
+	this.style.background = 'lightgray';
+})
+	q8.addEventListener('mouseleave',function(){
+	this.style.background = '';
+})
 
 
 // Q 9
+let q9 = document.querySelector('#q9-btn');
+	q9.addEventListener('click',function(){
+	let input =document.querySelector('#q9-input');
+	let result =document.querySelector('#q9-result');
+
+	result.textContent = input.value;
+})
 
 
 // Q 10
+let q10 = document.querySelector('.q10-btn');
+	q10.addEventListener('click',function(){
+	let input =document.querySelector('#q10-input');
+	let result =document.querySelector('#q10-result');
 
+	if(input.value < 20) {
+		result.textContent = '20歳未満の方の利用は禁止です。';
+	}else {
+		result.textContent = input.value;
+	}
+})
